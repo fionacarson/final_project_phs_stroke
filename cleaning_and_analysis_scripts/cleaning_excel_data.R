7981bd5460468e3348261f484274e911d68533ab
Fiona Carson <fionascarson@gmail.com>
2023-02-16 19:52:07 +0000

    day 4 updates

--- cleaning_and_analysis_scripts/cleaning_excel_data.R
+++ cleaning_and_analysis_scripts/cleaning_excel_data.R
@@ -10,3 +10,25 @@ incidence %>%
   select(where(is.character)) %>% 
   map(unique)
 
+# No missing values
+
+# TIDY AGE GROUP COLUMN
+incidence <- incidence %>% 
+  rename("age" = "ageband",
+         "diagnosis" = "cond",
+         "hbr_name" = "hb",
+         "sex" = "sexdesc",
+         "incidence" = "inc",
+         "crude_rate" = "crude") %>% 
+  mutate(age = recode(age, 
+                      "<75" = "under 75",
+                      "75+" = "over 75",
+                      "ALL AGES" = "All"),
+         sex = recode(sex, 
+                      "MALE" = "Male",
+                      "FEMALE" = "Female",
+                      "BOTH SEXES" = "All"))
+
+
+write_csv(incidence, here("clean_data/incidence.csv"))
+
